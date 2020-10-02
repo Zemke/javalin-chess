4464760daa0db4a23e6263d610315ca16528d668
Zemke <florian@zemke.io>
2020-10-02 21:03:29 +0200

    Figures

---
+++ src/main/kotlin/io/zemke/javalinchess/figure/Bishop.kt
@@ -0,0 +1,5 @@
+package io.zemke.javalinchess.figure
+
+import io.zemke.javalinchess.figure.Figure
+
+class Bishop : Figure()
---
+++ src/main/kotlin/io/zemke/javalinchess/figure/Figure.kt
@@ -0,0 +1,3 @@
+package io.zemke.javalinchess.figure
+
+open class Figure
