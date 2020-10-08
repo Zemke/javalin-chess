adf6ae7a9de4db48a8000b3800665bca2086ed9e
Zemke <florian@zemke.io>
2020-10-08 18:31:21 +0200

    Rook moving left and right with opponent pieces

--- src/main/kotlin/io/zemke/javalinchess/piece/Rook.kt
+++ src/main/kotlin/io/zemke/javalinchess/piece/Rook.kt
@@ -6,29 +6,64 @@ import io.zemke.javalinchess.controller.Player
 class Rook(player: Player, color: Color, position: Position) : Piece("Rook", player, color, position) {
 
     override fun allowedNextPositions(board: Board): List<Position> {
-        var allowedOnSameRank = mutableListOf<Position>()
         val current = board.findPositionOfPiece(this)
                 ?: throw RuntimeException("Position of Figure $this not found")
+        val allowedOnEmptyBoard = generallyAllowedForPiece(current)
+        val result = mutableListOf(*allowedOnEmptyBoard.toTypedArray())
+
+        // same rank
+        removeProhibitedPositions(result, current, board, (current.file + 1)..7, true) // files to the right
+        removeProhibitedPositions(result, current, board, (current.file - 1 downTo 0), true) // files to the left
+
+        // same file
+        removeProhibitedPositions(result, current, board, (current.rank + 1)..7, false) // ranks upward
+        removeProhibitedPositions(result, current, board, (current.rank - 1 downTo 0), false) // ranks downward
+
+        return result
+    }
+
+    private fun removeProhibitedPositions(
+            result: MutableList<Position>,
+            current: Position,
+            board: Board,
+            progression: IntProgression,
+            checkOnRank: Boolean) {
+        val ascending = progression.last > progression.first
+        for (fileOrRank in progression) {
+            val subjectPosition = when (checkOnRank) {
+                true -> Position(fileOrRank, current.rank)
+                false -> Position(current.file, fileOrRank)
+            }
+            val pieceAtPos = board.getPieceAt(subjectPosition)
+            if (pieceAtPos != null) {
+                if (pieceAtPos.color == color) {
+                    val progressionToRemove = IntProgression.fromClosedRange(
+                            fileOrRank, progression.last, if (ascending) +1 else -1)
+                    when (checkOnRank) {
+                        true -> result.removeIf { it.rank == current.rank && progressionToRemove.contains(it.file) }
+                        false -> result.removeIf { it.file == current.file && progressionToRemove.contains(it.rank) }
+                    }
+                } else {
+                    val progressionToRemove = IntProgression.fromClosedRange(
+                            fileOrRank + (if (ascending) +1 else -1), progression.last, if (ascending) +1 else -1)
+                    when (checkOnRank) {
+                        true -> result.removeIf { it.rank == current.rank && progressionToRemove.contains(it.file) }
+                        false -> result.removeIf { it.file == current.file && progressionToRemove.contains(it.rank) }
+                    }
+                }
+                break
+            }
+        }
+    }
+
+    private fun generallyAllowedForPiece(current: Position): List<Position> {
+        val allowedOnSameRank = mutableListOf<Position>()
         for (num in 0 until current.file) allowedOnSameRank.add(Position(num, current.rank))
         for (num in current.file..7) allowedOnSameRank.add(Position(num, current.rank))
-        allowedOnSameRank = allowedOnSameRank.filter { it != current }.toMutableList()
-        var rankBroken = false
-        allowedOnSameRank = allowedOnSameRank.filter {
-            if (rankBroken) false
-            val res = board.getPieceAt(it) == null
-            rankBroken = res;
-            res
-        }.toMutableList()
-        var allowedOnSameFile = mutableListOf<Position>()
+        val allowedOnSameFile = mutableListOf<Position>()
         for (num in 0 until current.rank) allowedOnSameFile.add(Position(current.file, num))
         for (num in current.rank..7) allowedOnSameFile.add(Position(current.file, num))
-        var fileBroken = false
-        allowedOnSameFile = allowedOnSameFile.filter {
-            if (fileBroken) false
-            val res = board.getPieceAt(it) == null
-            fileBroken = res;
-            res
-        }.toMutableList()
         return listOf(*allowedOnSameRank.toTypedArray(), *allowedOnSameFile.toTypedArray())
+                .filter { it != current }
     }
 }
--- src/test/kotlin/io/zemke/javalinchess/piece/RookTest.kt
+++ src/test/kotlin/io/zemke/javalinchess/piece/RookTest.kt
@@ -19,5 +19,41 @@ class RookTest {
                 Position(3, 0), Position(3, 1), Position(3, 2),
                 Position(3, 4), Position(3, 5), Position(3, 6), Position(3, 7))
     }
+
+    @Test
+    fun `allowed next positions when with opponent to the right`() {
+        val rook = Rook(Player("p1"), Color.BLACK, Position(3, 3))
+        val otherRook = Rook(Player("p2"), Color.WHITE, Position(5, 3))
+        val board = Board()
+        board.putPiece(rook)
+        board.putPiece(otherRook)
+        println(board)
+        val actual = rook.allowedNextPositions(board);
+        assertThat(actual).containsExactlyInAnyOrder(
+                Position(0, 3), Position(1, 3), Position(2, 3),
+                Position(4, 3), Position(5, 3), /*Position(6, 3), Position(7, 3),*/
+                Position(3, 0), Position(3, 1), Position(3, 2),
+                Position(3, 4), Position(3, 5), Position(3, 6), Position(3, 7))
+    }
+
+    @Test
+    fun `allowed next positions when with opponent to the left`() {
+        val rook = Rook(Player("p1"), Color.BLACK, Position(3, 3))
+        val otherRook = Rook(Player("p2"), Color.WHITE, Position(1, 3))
+        val board = Board()
+        board.putPiece(rook)
+        board.putPiece(otherRook)
+        println(board)
+        val actual = rook.allowedNextPositions(board);
+        assertThat(actual).containsExactlyInAnyOrder(
+                /*Position(0, 3), */Position(1, 3), Position(2, 3),
+                Position(4, 3), Position(5, 3), Position(6, 3), Position(7, 3),
+                Position(3, 0), Position(3, 1), Position(3, 2),
+                Position(3, 4), Position(3, 5), Position(3, 6), Position(3, 7))
+    }
+
+    // todo move upward
+
+    // todo move downward
 }
 
