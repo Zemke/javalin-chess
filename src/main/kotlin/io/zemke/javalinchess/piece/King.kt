032cf7242ab40ec12ef36ba546571f085a04508f
Zemke <florian@zemke.io>
2020-10-09 17:32:43 +0200

    Fix typo

--- src/main/kotlin/io/zemke/javalinchess/piece/King.kt
+++ src/main/kotlin/io/zemke/javalinchess/piece/King.kt
@@ -7,7 +7,7 @@ class King(player: Player, color: Color, position: Position) : Piece("King", pla
 
     override fun allowedNextPositions(board: Board): Set<Position> {
         val current = board.findPositionOfPiece(this)
-                ?: throw IllegalArgumentException("Pice $this is not on board.")
+                ?: throw IllegalArgumentException("Piece $this is not on board.")
         val ownPieces = board.ownPieces(this.color).map { it.position }
         return setOf(
                 Position(current.file + 1, current.rank),
