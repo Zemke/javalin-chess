7912f8775b668e1064e495ac5f20b44af7ef18e0
Zemke <florian@zemke.io>
2020-10-09 20:11:18 +0200

    Validate Position instantiation

--- src/main/kotlin/io/zemke/javalinchess/piece/Position.kt
+++ src/main/kotlin/io/zemke/javalinchess/piece/Position.kt
@@ -2,6 +2,11 @@ package io.zemke.javalinchess.piece
 
 class Position(val file: Int, val rank: Int) {
 
+    init {
+        if (!isValidPosition(file, rank))
+            throw IllegalArgumentException("Invalid position. (file=$file, rank=$rank)")
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -22,5 +27,9 @@ class Position(val file: Int, val rank: Int) {
 
     override fun toString(): String {
         return "Position(file=$file, rank=$rank)"
+    }
+
+    companion object {
+        fun isValidPosition(file: Int, rank: Int): Boolean = rank in 0..7 && file in 0..7
     }
 }
