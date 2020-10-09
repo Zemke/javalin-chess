8ba9b5b2604a3d1a644ca8deda0e37631d3cb056
Zemke <florian@zemke.io>
2020-10-09 17:10:40 +0200

    Knight implementation is actually King's

--- src/main/kotlin/io/zemke/javalinchess/piece/King.kt
+++ src/main/kotlin/io/zemke/javalinchess/piece/King.kt
@@ -4,7 +4,20 @@ import io.zemke.javalinchess.controller.Board
 import io.zemke.javalinchess.controller.Player
 
 class King(player: Player, color: Color, position: Position) : Piece("King", player, color, position) {
+
     override fun allowedNextPositions(board: Board): Set<Position> {
-        TODO("Not yet implemented")
+        val current = board.findPositionOfPiece(this)
+                ?: throw IllegalArgumentException("Pice $this is not on board.")
+        val ownPieces = board.ownPieces(this.color).map { it.position }
+        return setOf(
+                Position(current.file + 1, current.rank),
+                Position(current.file - 1, current.rank),
+                Position(current.file, current.rank + 1),
+                Position(current.file, current.rank - 1),
+                Position(current.file - 1, current.rank - 1),
+                Position(current.file - 1, current.rank + 1),
+                Position(current.file + 1, current.rank - 1),
+                Position(current.file + 1, current.rank + 1),
+        ).filter { !ownPieces.contains(it) }.toSet()
     }
 }
--- src/test/kotlin/io/zemke/javalinchess/piece/KingTest.kt
+++ src/test/kotlin/io/zemke/javalinchess/piece/KingTest.kt
@@ -2,18 +2,18 @@ package io.zemke.javalinchess.piece
 
 import io.zemke.javalinchess.controller.Board
 import io.zemke.javalinchess.controller.Player
-import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
-class KnightTest {
+class KingTest {
 
     @Test
     fun `next allowed positions on an empty board`() {
         val board = Board()
-        val knight = Knight(Player("p1"), Color.BLACK, Position(3, 3))
-        board.putPiece(knight)
-        val actual = knight.allowedNextPositions(board)
-        assertThat(actual).containsExactlyInAnyOrder(
+        val king = King(Player("p1"), Color.BLACK, Position(3, 3))
+        board.putPiece(king)
+        val actual = king.allowedNextPositions(board)
+        Assertions.assertThat(actual).containsExactlyInAnyOrder(
                 Position(3, 4), Position(3, 2), Position(2, 3), Position(4, 3),
                 Position(2, 2), Position(4, 4), Position(4, 2), Position(2, 4))
     }
@@ -23,15 +23,15 @@ class KnightTest {
         val board = Board()
         val p1 = Player("p1")
         val p2 = Player("p2")
-        val knight = Knight(p1, Color.BLACK, Position(3, 3))
+        val king = King(p1, Color.BLACK, Position(3, 3))
         setOf(
                 Rook(p2, Color.WHITE, Position(2, 1)),
                 Rook(p1, Color.BLACK, Position(2, 3)),
                 Rook(p2, Color.WHITE, Position(4, 4)))
                 .forEach { board.putPiece(it) }
-        board.putPiece(knight)
-        val actual = knight.allowedNextPositions(board)
-        assertThat(actual).containsExactlyInAnyOrder(
+        board.putPiece(king)
+        val actual = king.allowedNextPositions(board)
+        Assertions.assertThat(actual).containsExactlyInAnyOrder(
                 Position(3, 4), Position(3, 2), /*Position(2, 3), */Position(4, 3),
                 Position(2, 2), Position(4, 4), Position(4, 2), Position(2, 4))
     }
