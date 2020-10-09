4fa71d61cce0f30713ecc215c8a014c5ec5729b0
Zemke <florian@zemke.io>
2020-10-09 11:07:25 +0200

    Tests for moving up and down

--- src/test/kotlin/io/zemke/javalinchess/piece/RookTest.kt
+++ src/test/kotlin/io/zemke/javalinchess/piece/RookTest.kt
@@ -52,8 +52,35 @@ class RookTest {
                 Position(3, 4), Position(3, 5), Position(3, 6), Position(3, 7))
     }
 
-    // todo move upward
+    @Test
+    fun `allowed next positions when with opponent to the top`() {
+        val rook = Rook(Player("p1"), Color.BLACK, Position(3, 3))
+        val otherRook = Rook(Player("p2"), Color.WHITE, Position(3, 1))
+        val board = Board()
+        board.putPiece(rook)
+        board.putPiece(otherRook)
+        println(board)
+        val actual = rook.allowedNextPositions(board);
+        assertThat(actual).containsExactlyInAnyOrder(
+                Position(0, 3), Position(1, 3), Position(2, 3),
+                Position(4, 3), Position(5, 3), Position(6, 3), Position(7, 3),
+                /*Position(3, 0), */Position(3, 1), Position(3, 2),
+                Position(3, 4), Position(3, 5), Position(3, 6), Position(3, 7))
+    }
 
-    // todo move downward
+    @Test
+    fun `allowed next positions when with opponent to the bottom`() {
+        val rook = Rook(Player("p1"), Color.BLACK, Position(3, 3))
+        val otherRook = Rook(Player("p2"), Color.WHITE, Position(3, 5))
+        val board = Board()
+        board.putPiece(rook)
+        board.putPiece(otherRook)
+        println(board)
+        val actual = rook.allowedNextPositions(board);
+        assertThat(actual).containsExactlyInAnyOrder(
+                Position(0, 3), Position(1, 3), Position(2, 3),
+                Position(4, 3), Position(5, 3), Position(6, 3), Position(7, 3),
+                Position(3, 0), Position(3, 1), Position(3, 2),
+                Position(3, 4), Position(3, 5)/*, Position(3, 6), Position(3, 7)*/)
+    }
 }
-
