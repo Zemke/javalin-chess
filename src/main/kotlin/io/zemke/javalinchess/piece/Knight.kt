a26fad37d1785ea91f2253bf10b924890cc02ed0
Zemke <florian@zemke.io>
2020-10-10 19:10:41 +0200

    Movements of a Knight

--- src/main/kotlin/io/zemke/javalinchess/piece/Knight.kt
+++ src/main/kotlin/io/zemke/javalinchess/piece/Knight.kt
@@ -5,6 +5,18 @@ import io.zemke.javalinchess.controller.Board
 class Knight(color: Color, position: Position) : Piece("Knight", color, position) {
 
     override fun allowedNextPositions(board: Board): Set<Position> {
-        TODO("Not yet implemented")
+        val current = board.findPositionOfPiece(this)
+                ?: throw RuntimeException("Position of Piece $this not found")
+        val ownPieces = board.ownPieces(this.color).map { it.position }
+        return setOf(
+                Position(current.file + 2, current.rank - 1),
+                Position(current.file + 2, current.rank + 1),
+                Position(current.file - 2, current.rank - 1),
+                Position(current.file - 2, current.rank + 1),
+                Position(current.file - 1, current.rank + 2),
+                Position(current.file + 1, current.rank + 2),
+                Position(current.file - 1, current.rank - 2),
+                Position(current.file + 1, current.rank - 2),
+        ).filter { !ownPieces.contains(it) }.toSet()
     }
 }
---
+++ src/test/kotlin/io/zemke/javalinchess/piece/KnightTest.kt
@@ -0,0 +1,37 @@
+package io.zemke.javalinchess.piece
+
+import io.zemke.javalinchess.controller.Board
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class KnightTest {
+
+    @Test
+    fun `allowed positions on an empty board`() {
+        val knight = Knight(Color.BLACK, Position(3, 3))
+        val board = Board(false)
+        board.putPiece(knight)
+        val actual = knight.allowedNextPositions(board)
+        assertThat(actual).containsExactlyInAnyOrder(
+                Position(5, 2), Position(5, 4), Position(1, 2), Position(1, 4),
+                Position(2, 5), Position(4, 5), Position(2, 1), Position(4, 1))
+    }
+
+    @Test
+    fun `allowed positions on a complex board`() {
+        val knight = Knight(Color.BLACK, Position(3, 3))
+        val board = Board(false)
+        board.putPiece(knight)
+        setOf(
+                Knight(Color.BLACK, Position(5, 4)),
+                Knight(Color.WHITE, Position(2, 5)),
+                Knight(Color.BLACK, Position(4, 4)),
+                Knight(Color.WHITE, Position(2, 2)),
+        )
+                .forEach { board.putPiece(it) }
+        val actual = knight.allowedNextPositions(board)
+        assertThat(actual).containsExactlyInAnyOrder(
+                Position(5, 2), /*Position(5, 4), */Position(1, 2), Position(1, 4),
+                Position(2, 5), Position(4, 5), Position(2, 1), Position(4, 1))
+    }
+}
