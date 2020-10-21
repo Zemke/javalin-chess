ec0a904a0f634500d6d3fef73b7a7cf8e8edfa24
Zemke <florian@zemke.io>
2020-10-21 15:18:46 +0200

    Knight filter board exceeding positions

--- src/main/kotlin/io/zemke/javalinchess/chess/piece/Knight.kt
+++ src/main/kotlin/io/zemke/javalinchess/chess/piece/Knight.kt
@@ -8,14 +8,16 @@ class Knight(color: Color, position: Position) : Piece("Knight", color, position
         val current = board.findPositionOfPiece(this)
         val ownPieces = board.ownPieces(this.color).map { it.position }
         return setOf(
-                Position(current.file + 2, current.rank - 1),
-                Position(current.file + 2, current.rank + 1),
-                Position(current.file - 2, current.rank - 1),
-                Position(current.file - 2, current.rank + 1),
-                Position(current.file - 1, current.rank + 2),
-                Position(current.file + 1, current.rank + 2),
-                Position(current.file - 1, current.rank - 2),
-                Position(current.file + 1, current.rank - 2),
-        ).filter { !ownPieces.contains(it) }.toSet()
+                Position.ifValid(current.file + 2, current.rank - 1),
+                Position.ifValid(current.file + 2, current.rank + 1),
+                Position.ifValid(current.file - 2, current.rank - 1),
+                Position.ifValid(current.file - 2, current.rank + 1),
+                Position.ifValid(current.file - 1, current.rank + 2),
+                Position.ifValid(current.file + 1, current.rank + 2),
+                Position.ifValid(current.file - 1, current.rank - 2),
+                Position.ifValid(current.file + 1, current.rank - 2),
+        )
+                .filterNotNull()
+                .filter { !ownPieces.contains(it) }.toSet()
     }
 }
