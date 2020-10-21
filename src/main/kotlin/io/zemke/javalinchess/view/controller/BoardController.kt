692016942135cd29c771f2b09f8aa9b0a849eca4
Zemke <florian@zemke.io>
2020-10-21 13:24:20 +0200

    User DelegationContext in BoardController

--- src/main/kotlin/io/zemke/javalinchess/view/controller/BoardController.kt
+++ src/main/kotlin/io/zemke/javalinchess/view/controller/BoardController.kt
@@ -2,6 +2,7 @@ package io.zemke.javalinchess.view.controller
 
 import io.javalin.http.Context
 import io.javalin.http.InternalServerErrorResponse
+import io.zemke.javalinchess.DelegationContext
 import io.zemke.javalinchess.aspectj.annotations.Inject
 import io.zemke.javalinchess.aspectj.annotations.Zemke
 import io.zemke.javalinchess.chess.Board
@@ -13,7 +14,8 @@ class BoardController {
     @Inject
     private lateinit var memcached: Memcached
 
-    fun create(ctx: Context) {
+    fun create(_ctx: Context) {
+        val ctx = DelegationContext(_ctx)
         val board = Board(true)
         board.uuidWhite = ctx.header("auth")
         when (memcached.store(board.id, board)) {
--- src/test/kotlin/io/zemke/javalinchess/view/controller/BoardControllerTest.kt
+++ src/test/kotlin/io/zemke/javalinchess/view/controller/BoardControllerTest.kt
@@ -1,13 +1,11 @@
 package io.zemke.javalinchess.view.controller
 
 import io.javalin.http.Context
-import io.mockk.every
+import io.mockk.*
 import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.OverrideMockKs
 import io.mockk.junit5.MockKExtension
-import io.mockk.mockk
-import io.mockk.slot
-import io.mockk.verify
+import io.zemke.javalinchess.DelegationContext
 import io.zemke.javalinchess.chess.Board
 import io.zemke.javalinchess.complex.Memcached
 import org.assertj.core.api.Assertions.assertThat
@@ -27,12 +25,13 @@ class BoardControllerTest {
     @Test
     fun `POST creates valid board`() {
         val ctx = mockk<Context>(relaxed = true)
-        every { ctx.header("auth") } returns UUID.randomUUID().toString()
+        mockkConstructor(DelegationContext::class)
+        every { anyConstructed<DelegationContext>().header("auth") } returns UUID.randomUUID().toString()
         val boardIdSlot = slot<String>()
         val boardSlot = slot<Board>()
         val resJsonSlot = slot<Board>()
         every { memcached.store(capture(boardIdSlot), capture(boardSlot)) } returns true
-        every { ctx.json(capture(resJsonSlot)) } returns ctx
+        every { anyConstructed<DelegationContext>().json(capture(resJsonSlot))} returns ctx
         boardController.create(ctx)
         assertThat(boardIdSlot.captured).isEqualTo(resJsonSlot.captured.id)
         assertThat(boardSlot.captured).isEqualTo(resJsonSlot.captured)
